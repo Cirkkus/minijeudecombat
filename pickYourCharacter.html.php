b2f784478b977fd7a1d23512a590c06982f8ff5d
Alex Chaval <alex.chaval@laposte.net>
2019-06-18 23:40:10 +0200

    Page pickYourCharacter + flexbox

--- pickYourCharacter.html.php
+++ pickYourCharacter.html.php
@@ -10,10 +10,16 @@
     <div class="box"> 
         <h1> L'Arene d'Anzu </h1>
         <p> Bienvenue <?php echo $_POST["pseudo"] ?>
-
+    
         <h2> Choissisez votre Combattant !<h2>
-            
+    </div>    
+    <div class="personnages">
+        <div class="perso1">  </div>
+        <div class="perso2">  </div>
+        <div class="perso3">  </div>
+        <div class="perso4">  </div>
     </div>
+    
 
 </body>
 </html>
