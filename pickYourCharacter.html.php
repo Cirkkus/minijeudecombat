4a9bda79d06cb204358b60721483e6e4ad4351d8
Alex Chaval <alex.chaval@laposte.net>
2019-06-18 22:45:44 +0200

    Premier commit, structure visuel du site

---
+++ pickYourCharacter.html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8";>
+    <link rel="stylesheet" href="style.css">
+    <title> Fight Club </title>
+</head>
+<body>
+
+    <div class="box"> 
+        <h1> L'Arene d'Anzu </h1>
+        <p> Bienvenue <?php echo $_POST["pseudo"] ?>
+
+        <h2> Choissisez votre Combattant !<h2>
+            
+    </div>
+
+</body>
+</html>
