054bf0c10f19d1405e309cf9d578c1b85b171b72
Alex Chaval <alex.chaval@laposte.net>
2019-06-19 23:58:15 +0200

    création Getter & setter

--- Model/Classes/Character.php
+++ Model/Classes/Character.php
@@ -6,14 +6,89 @@
         private $name;
         private $life;
         private $strength;
-        private $classe;
+        private $exp;
+        private $level;
+      
 
-        CONST CHARACTER_NAME = ["Kael'Thas","Zeratul","Valyria","Grisetête"];
+       
 
         public function __construct(){
+            $this->$id = $id;
             $this->name = $name;
             $this->life = $life;
             $this->strength = $strength;
-            $this->classe = $classe;
+            $this->exp = $exp;
+            $this->level = $level;
+         
         }
+
+        public function getId(){
+            return $this->$id;
+        }
+    
+
+        public function getName(){
+            return $this->$name;
+        }
+    
+
+        public function getLife(){
+            return $this->$life;
+        }
+    
+
+        public function getStrength(){
+            return $this->$strength;
+        }
+    
+
+        public function getExp(){
+            return $this->$exp;
+        }
+
+        public function getLevel(){
+            return $this->$level;
+        }
+
+        public function setId($id){
+            $id = (int) $id;
+            if ($id >= 0 && $id < 999) {
+                $this->id = $id;}
+        } 
+
+        public function setName($name){
+            if (is.string($name)) {
+                $this->name = $name;
+            } 
+        }
+
+        public function setLife($life) {
+             $life = (int) $life;
+            if ($life >= 0 && $life <= 200) {
+                $this->life = $life;
+            } 
+        }
+
+        public function setStrength($strength){
+            $strength = (int) $strength;
+            if ($strength >= 0 && $strength <= 50) {
+                $this->strength = $strength;
+            } 
+        } 
+
+        public function setExp($exp){
+            $exp = (int) $exp;
+            if ($exp >= 0 && $exp <= 100) {
+                $this->exp = $exp;
+            } 
+        } 
+        
+        public function setLevel($level){
+            $level = (int) $level;
+            if ($level >= 0 && $level <= 50) {
+                $this->level = $level;
+            } 
+        } 
+   
+   
     }
